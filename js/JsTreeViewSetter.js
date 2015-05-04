1966431f14989faca6cc5acb89bae2be6ed7d9b4
Daniel Maatari Okouya <maatariokouya@gmail.com>
2015-05-03 22:08:22 -0400

    Adjust to behave more like in BRIK
    -> One click do select but avoid the color that comes with activation.
    
    -> Click on the exanding icon only expand the sub-tree it does not select the item

--- js/JsTreeViewSetter.js
+++ js/JsTreeViewSetter.js
@@ -11,13 +11,60 @@ function setPoolPartyTreeBroswer($input){
 
             icons:false,
 
+            selectMode: 1,
+
+            focusOnSelect: false,
+
+
+
             lazyLoad: function(event, data){
 
                 var node = data.node;
                 // Load child nodes via ajax GET /getTreeData?mode=children&parent=1234
 
                 data.result = doPoolPartyGetChildrenAjaxRequest(data.node.data.uri);
+            },
+
+            beforeActivate: function(event, data) {
+                alert(data.node.data.uri)
+                return false;
+            },
+
+            activate: function(event, data) {
+                return false;
+            },
+
+            click: function(event, data) {
+                data.node.toggleSelected()
+            },
+
+            beforeSelect: function(event, data) {
+                return false;
             }
+
+            /*focus: function(event, data) {
+                console.log("in focus")
+                return false
+            },
+
+            focusTree: function(event, data) {
+                console.log("in focusTree")
+                return false
+            },*/
+
+
+
+
+
+            /*select: function(event, data){
+                // A node is about to be selected: prevent this for folders:
+                alert(data.node.data.uri)
+            },
+
+            beforeSelect: function(event, data){
+                // A node is about to be selected: prevent this for folders:
+                alert(data.node.data.uri)
+            }*/
         }
     )
 };
@@ -30,7 +77,7 @@ function doPoolPartyGetChildrenAjaxRequest(parent) {
 
         url: "http://thesaurus.iadb.org/PoolParty/api/thesaurus/1DCE031A-B429-0001-49B6-15A028B01F5D/childconcepts",
 
-        data: {language: "en", parent: parent},
+        data: {language: "en", parent: parent, properties: "skos:narrower"},
 
         username: 'superadmin',
         password: 'poolparty',
@@ -55,7 +102,10 @@ function doPoolPartyGetChildrenAjaxRequest(parent) {
 
             for (var i = 0; i < data.length; i++) {
                 data[i].title = data[i].prefLabel
-                data[i].lazy = true
+
+                if (!(data[i].narrowers === undefined)) {
+                    data[i].lazy = true
+                }
             }
 
         }
